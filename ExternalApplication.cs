f0f742696c06c3933bfd12c2a15588e02c3d1bb3
Max Sun <465565873@qq.com>
2019-07-22 14:02:24 +0930

    Add project files.

---
+++ ExternalApplication.cs
@@ -0,0 +1,44 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace LucidToolbar
+{
+    class ExternalApplication : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            //create ribbon tab
+            application.CreateRibbonTab("Lucid Tools");
+
+            string path = Assembly.GetExecutingAssembly().Location;
+
+            PushButtonData button = new PushButtonData("HydraulicTools", "PlacePipeElement", path, "LucidToolbar.PlacePipeElement");
+
+            RibbonPanel panel = application.CreateRibbonPanel("Lucid Tools", "Hydraulic");
+
+            //Add button image
+            Uri imagepath = new Uri(@"C:\Users\Max.Sun\Desktop\Revit Folder\tools.png");
+       
+            BitmapImage image = new BitmapImage(imagepath);
+
+            PushButton pushButton = panel.AddItem(button) as PushButton;
+            pushButton.LargeImage = image;
+            //S:\Bluesky\Project Files\RevitTools[Testing]\output - onlinepngtools.png
+
+            return Result.Succeeded;
+
+        }
+    }
+}
---
+++ PlacePipeElement.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB.Plumbing;
+
+
+namespace LucidToolbar
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+
+    public class PlacePipeElement : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get UIDocument
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            //Get Document
+            Document doc = uidoc.Document;
+
+            //Get Level
+            Level level = new FilteredElementCollector(doc)
+            .OfCategory(BuiltInCategory.OST_Levels)
+            .WhereElementIsNotElementType()
+            .Cast<Level>()
+            .First(x => x.Name == "Ground Floor");
+
+            //Get Family Symbol
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_PipingSystem).Where(ps => ps.Name == "Domestic Cold Water");
+
+            // Create piping system
+            FilteredElementCollector sysCollector = new FilteredElementCollector(doc);
+            sysCollector.OfClass(typeof(PipingSystemType));
+            ElementId pipeSysTypeId = sysCollector.FirstElementId();
+
+            //Get pipetype
+            FilteredElementCollector collector1 = new FilteredElementCollector(doc);
+
+            PipeType pipeType = collector1.OfClass(typeof(PipeType))
+            .WhereElementIsElementType()
+            .Cast<PipeType>()
+            .First(x => x.Name == "ABS - Solvent Welded_BMA");
+
+            //Try Catch Condition
+            try
+            {
+                using (Transaction trans = new Transaction(doc, "Place Family"))
+                {
+                    if (null != pipeType)
+                    {
+                        trans.Start();
+                        //Create Pipe 
+                        Pipe.Create(doc, pipeSysTypeId, pipeType.Id, level.Id, new XYZ(0, 0, 0), new XYZ(100, 0, 0));
+                        trans.Commit();
+                    }
+                }
+                return Result.Succeeded;
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                return Result.Failed;
+            }
+        }
+    }
+}
